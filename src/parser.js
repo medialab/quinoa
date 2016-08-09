5a40755bcfcdfbe26413fa94330ffa746290a9e5
Yomguithereal <guillaumeplique@gmail.com>
2016-08-09 17:56:18 +0200

    Removing empty file

--- src/parser.js
+++
@@ -1,7 +0,0 @@
-/**
- * Quinoa Markdown Parser
- * =======================
- *
- * Quinoa's extended markdown parser that will parse the markdown string and
- * output a proper list of slides & their attached information.
- */
