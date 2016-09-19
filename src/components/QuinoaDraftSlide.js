c6050c7ced6ddb611c9550c82ec2ea0784b306bf
Yomguithereal <guillaumeplique@gmail.com>
2016-09-19 19:29:18 +0200

    Adding rich utils to draft

--- src/components/QuinoaDraftSlide.js
+++ src/components/QuinoaDraftSlide.js
@@ -5,11 +5,22 @@
  * Component rendering the Draft.js editor for a single slide.
  */
 import React, {Component} from 'react';
-import {Editor} from 'draft-js';
+import {Editor, RichUtils} from 'draft-js';
 
 import QuinoaSlideTitle from './QuinoaSlideTitle';
 
 export default class QuinoaDraftSlide extends Component {
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.props.draft, command);
+
+    if (newState) {
+      this.props.update(this.props.id, {draft: newState});
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
   render() {
     const {
       id,
@@ -26,7 +37,8 @@ export default class QuinoaDraftSlide extends Component {
         </div>
         <Editor
           editorState={draft}
-          onChange={state => update(id, {draft: state})} />
+          onChange={state => update(id, {draft: state})}
+          handleKeyCommand={this.handleKeyCommand.bind(this)} />
       </div>
     );
   }
